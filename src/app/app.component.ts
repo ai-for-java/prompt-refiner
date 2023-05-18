13314bbe0424652aeac00c2b22ab330a68e28e25
deep-learning-dynamo <deep.learning.dynamo@gmail.com>
2023-05-18 18:56:58 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NgZone } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 interface APIResponse {
   id: string;
@@ -19,6 +22,9 @@ interface APIResponse {
         content: string;
       };
       finish_reason: string;
+      delta: {
+        content: string;
+      };
     }
   ];
 }
@@ -41,10 +47,10 @@ interface Input {
 })
 export class AppComponent {
   title = 'Prompt Refiner';
-  responses: String[] = [];
+
   config = {
     model: 'gpt-3.5-turbo',
-    temperature: 0.0
+    temperature: 0.0,
   };
   prompt: Prompt = {
     userPrompt: '',
@@ -52,11 +58,10 @@ export class AppComponent {
     userPromptInputs: [],
   };
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient, private zone: NgZone) {}
 
   addInput() {
-    this.prompt.userPromptInputs.push({id: '', text: ''});
+    this.prompt.userPromptInputs.push({ id: '', text: '' });
   }
 
   removeInput(index: number) {
@@ -69,10 +74,6 @@ export class AppComponent {
       finalPrompt = finalPrompt.replace(`\${${input.id}}`, input.text);
     }
 
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer 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`,
-    };
     const messages = [
       {
         role: 'user',
@@ -86,32 +87,79 @@ export class AppComponent {
       model: this.config.model,
       messages,
       temperature: +this.config.temperature,
+      stream: true
     };
 
     // Log headers and body
-    console.log('Request Headers:', headers);
-    console.log('Request Body:', body);
+    console.log('Request: ', body);
 
-    this.http
-      .post<APIResponse>('https://api.openai.com/v1/chat/completions', body, {headers})
-      .subscribe(
-        (data: APIResponse) => {
-          const responseContent = data.choices[0]?.message;
-          if (responseContent) {
-            this.responses.unshift(data.choices[0].message.content +
-              '\n\n' +
-              '################################################################\n' +
-              '################################################################\n\n'
-              + 'System Prompt: ' + this.prompt.systemPrompt + '\n'
-              + 'Prompt: ' + finalPrompt + '\n'
-              + 'Model: ' + this.config.model + '\n'
-              + 'Temperature: ' + this.config.temperature);
-          }
-          console.log('Response:', data);
+    this.responses.unshift('')
+
+    this.chatStream(
+      'https://api.openai.com/v1/chat/completions',
+      body,
+      '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'
+    ).subscribe();
+  }
+
+  responses: string[] = [];
+
+  // ...
+  chatStream(url: string, body: any, apikey: string) {
+    const self = this; // Capture reference to 'this'
+    return new Observable<string>(observer => {
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apikey}`,
         },
-        (error) => {
-          console.error(error);
+      }).then(response => {
+        const reader = response.body?.getReader();
+        const decoder = new TextDecoder();
+        if (!response.ok) {
+          console.info(response)
+          // handle response error
+          observer.error();
         }
-      );
+
+        function push() {
+          return reader?.read().then(({ done, value }) => {
+            if (done) {
+              observer.complete();
+              return;
+            }
+
+            //parse text content from response
+            const events = decoder.decode(value).split('\n\n');
+            let content = '';
+            for (let i = 0; i < events.length; i++) {
+              const event = events[i];
+              if (event === 'data: [DONE]') break;
+              if (event && event.slice(0, 6) === 'data: ') {
+                const data = JSON.parse(event.slice(6));
+                let content1 = data.choices[0].delta?.content || '';
+                console.log(content1);
+                self.responses[0] += content1; // Use the captured reference 'self'
+                content += content1;
+              }
+            }
+            observer.next(content);
+            push();
+          }).catch((err: Error) => {
+            // handle fetch error
+            console.error(err)
+            observer.error();
+          });
+        }
+
+        push();
+      }).catch((err: Error) => {
+        // handle fetch error
+        console.error(err)
+        observer.error();
+      });
+    });
   }
 }
